77b889842f83994cca52805c1965ffcd387cf93b
Anastron <benz.softwaredeveloping@gmail.com>
2014-06-12 14:21:31 +0200

    added Libgdx

---
+++ desktop/src/com/samsoftwaredevelopment/bouncingSpace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.samsoftwaredevelopment.bouncingSpace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.samsoftwaredevelopment.bouncingSpace.BouncingSpace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BouncingSpace(), config);
+	}
+}
